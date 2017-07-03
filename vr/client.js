df337ad9e9c464b27f90db9e16689c94ea9ceed2
Kaizeras <=>
2017-07-03 18:53:42 +0300

    0.0.4

--- vr/client.js
+++ vr/client.js
@@ -3,20 +3,20 @@
 // If you want to modify your application, start in "index.vr.js"
 
 // Auto-generated content.
-import {VRInstance} from 'react-vr-web';
-
-function init(bundle, parent, options) {
-  const vr = new VRInstance(bundle, 'centrovrui', parent, {
-    // Add custom options here
-    åenableHotReload: true,
-    ...options,
-  });
-  vr.render = function() {
-    // Any custom behavior you want to perform on each frame goes here
-  };
-  // Begin the animation loop
-  vr.start();
-  return vr;
-}
-
-window.ReactVR = {init};
+// import {VRInstance} from 'react-vr-web';
+//
+// function init(bundle, parent, options) {
+//   const vr = new VRInstance(bundle, 'centrovrui', parent, {
+//     // Add custom options here
+//     åenableHotReload: true,
+//     ...options,
+//   });
+//   vr.render = function() {
+//     // Any custom behavior you want to perform on each frame goes here
+//   };
+//   // Begin the animation loop
+//   vr.start();
+//   return vr;
+// }
+//
+// window.ReactVR = {init};
