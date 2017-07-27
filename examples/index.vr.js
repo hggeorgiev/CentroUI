131073560a1f0534ba6c6db1d0dafe18ca3f7a0d
Kaizeras <=>
2017-07-27 14:14:40 +0300

    Changed default values of NavItem

--- examples/index.vr.js
+++ examples/index.vr.js
@@ -1,29 +1,21 @@
 import React from 'react';
 import { AppRegistry, Text } from 'react-vr';
 import Container from '../components/core/container';
-import View from '../components/core/view';
-import Button from '../components/button/button';
-import CnIcon from '../components/button/icon';
-import CnCardContent from '../components/card/card-content';
-import CnCardFooter from '../components/card/card-footer';
-import CnCardHeader from '../components/card/card-header';
+import Navbar from '../components/navbar/navbar'
+import NavItem from '../components/navbar/nav-item'
+
 
 
 export default class Example extends React.Component {
     render() {
         return (
             <Container>
-                <View row flex>
-                    <View vcenter hcenter bg="red" flex>
-                        <Text style={{color: '#000'}}>dsadas</Text>
-                    </View>
-                    <View vcenter hcenter bg="green" flex={2}>
-                        <Text style={{color: '#000'}}>dsadas</Text>
-                    </View>
-                    <View vcenter hcenter bg="yellow">
-                        <Text style={{color: '#000'}}>dsadas</Text>
-                    </View>
-                </View>
+                <Navbar>
+                    <NavItem>
+                        <Text>Test</Text>
+                    </NavItem>
+                </Navbar>
+
             </Container>
         );
     }
